990242bdb92c1225967a101115c72b3b280a41e3
Mie Haga <miehaga2@gmail.com>
2024-07-08 11:29:39 -0700

    Fix login routing bug

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import Chat from "@/components/Chat";
 import WeeklyCalendar from "@/components/WeeklyCalendar";
 import { AiCompanion } from "@/components/AiCompanion";
 
 export default function Home() {
   const [messages, setMessages] = useState("Hi, I am Ai Companion.");
+  const [tasks, setTasks] = useState([] as { id: number; title: string }[]);
+  const path = usePathname();
+  const router = useRouter();
   const handleSendMessage = (newMessage: string) => {
     setMessages(newMessage);
   };
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("/api/tasks");
+      console.log(response);
+      if (response.status === 401) {
+        return router.push("/login");
+      }
+
+      setTasks(await response.json());
+    })();
+  }, []);
 
   return (
     <>
