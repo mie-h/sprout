74507d54b168a8844c74cf739e2749530d50cb27
Kazuki Kijima <kazukiyoshida96@gmail.com>
2024-07-17 16:15:32 -0700

    :sparkles: Add PostgreSQL connection in FastAPI app

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -3,6 +3,7 @@ import json
 from http import HTTPStatus
 from typing import Annotated
 
+import asyncpg
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from fastapi import Depends, FastAPI, HTTPException
 from loguru import logger
@@ -13,8 +14,19 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 
 
+# TODO: Create a dedicated configuration file to load the environment variables
+#       and import them here
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+# default values are provided for local development
+DATABASE_URL = (
+    "postgresql://"
+    f"{os.getenv('POSTGRES_USERNAME', 'postgres')}"
+    f":{os.getenv('POSTGRES_PASSWORD', 'postgres')}"
+    f"@{os.getenv('POSTGRES_HOSTNAME', 'db')}"  # container name in docker-compose
+    f":{os.getenv('POSTGRES_PORT', 5432)}"
+    f"/{os.getenv('POSTGRES_DATABASE', 'postgres')}"
+)
 
 
 app = FastAPI()
@@ -30,6 +42,22 @@ oauth.register(
     client_secret=GOOGLE_CLIENT_SECRET,
     client_kwargs={"scope": "openid email profile"},
 )
+
+
+@app.on_event("startup")
+async def startup():
+    app.state.pool = await asyncpg.create_pool(DATABASE_URL)
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.pool.close()
+
+
+# Dependency to get the database connection
+async def get_db_connection():
+    async with app.state.pool.acquire() as connection:
+        yield connection
 
 
 @app.get("/api")
