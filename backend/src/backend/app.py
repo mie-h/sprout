9a353bfb0ac2bcc7b03b1650c12f4de3a1eb11ad
Kazuki Kijima <39410438+huyfififi@users.noreply.github.com>
2024-07-17 20:41:23 -0700

    Configure database connection between FastAPI <-> PostgreSQL (#40)
    ## I'm submitting a ...
    
    - [ ] bug report
    - [x] feature request
    
    ## What is the current behavior?
    
    The database connection has not been configured.
    
    ## What is the expected behavior?
    
    FastAPI should be able to query the database #28

--- backend/src/backend/app.py
+++ backend/src/backend/app.py
@@ -1,8 +1,10 @@
 import os
 import json
+from contextlib import asynccontextmanager
 from http import HTTPStatus
-from typing import Annotated
+from typing import Annotated, Optional
 
+import asyncpg
 from authlib.integrations.starlette_client import OAuth, OAuthError
 from fastapi import Depends, FastAPI, HTTPException
 from loguru import logger
@@ -13,11 +15,33 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, RedirectResponse
 
 
+# TODO: Create a dedicated configuration file to load the environment variables
+#       and import them here
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+# default values are provided for local development
+DATABASE_URL = (
+    "postgresql://"
+    f"{os.getenv('POSTGRES_USERNAME', 'postgres')}"
+    f":{os.getenv('POSTGRES_PASSWORD', 'postgres')}"
+    f"@{os.getenv('POSTGRES_HOSTNAME', 'db')}"  # container name in docker-compose
+    f":{os.getenv('POSTGRES_PORT', 5432)}"
+    f"/{os.getenv('POSTGRES_DATABASE', 'postgres')}"
+)
 
 
-app = FastAPI()
+db_pool: Optional[asyncpg.pool.Pool] = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global db_pool
+    db_pool = await asyncpg.create_pool(DATABASE_URL)
+    yield
+    await db_pool.close()
+
+
+app = FastAPI(lifespan=lifespan)
 app.add_middleware(SessionMiddleware, secret_key="!secret")
 
 oauth = OAuth()
@@ -30,6 +54,12 @@ oauth.register(
     client_secret=GOOGLE_CLIENT_SECRET,
     client_kwargs={"scope": "openid email profile"},
 )
+
+
+# Dependency to get the database connection
+async def get_db_connection():
+    async with db_pool.acquire() as connection:
+        yield connection
 
 
 @app.get("/api")
@@ -83,6 +113,20 @@ def get_user_id(request: Request):
 async def get_tasks(user_id: Annotated[str, Depends(get_user_id)]):
     del user_id
     return [{"id": 1, "title": "Buy Milk"}, {"id": 2, "title": "Buy Bread"}]
+
+
+# just to demonstrate the db connection
+# TODO: Remove this endpoint once we have the actual endpoints
+# ref: https://magicstack.github.io/asyncpg/current/api/index.html#asyncpg.connection.Connection
+@app.get("/test-db")
+async def test_db(conn=Depends(get_db_connection)):
+    assert not conn.is_closed()
+    try:
+        result = await conn.fetch("SELECT * FROM database_that_does_not_exist")
+    except asyncpg.exceptions.UndefinedTableError as err:
+        result = str(err)
+    assert result == 'relation "database_that_does_not_exist" does not exist'
+    return {"result": result}
 
 
 if __name__ == "__main__":
